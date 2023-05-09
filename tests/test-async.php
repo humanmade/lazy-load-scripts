62c6c2934262e949991c1a90224445269f0b2424
Ivan Kristianto <ivan@ivankristianto.com>
2023-05-09 10:53:24 +0700

    Add initial tests files

---
+++ tests/test-async.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * PHPUnit bootstrap file.
+ */
+
+/**
+ * Test Async Script.
+ *
+ * @package LLS
+ */
+class Test_Async_Script extends WP_UnitTestCase {
+
+	/**
+	 * Test Tes
+	 *
+	 * @return void
+	 * @throws InvalidArgumentException
+	 * @throws ExpectationFailedException
+	 */
+	public function test_add_async_attribute() {
+		$this->assertTrue( true );
+	}
+}
